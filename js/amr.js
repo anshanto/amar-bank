f66a1d10eda36927fb483f473386beb6f9215e88
anshanto <asikurjamann@gmail.com>
2022-06-16 17:31:55 +0600

    all ok

--- js/amr.js
+++ js/amr.js
@@ -1,31 +1,28 @@
 function getInputValue(inputId) {
     const inputField = document.getElementById(inputId);
     const inputAmountText = inputField.value;
-    const inputValue = parseFloat(inputAmountText);
+    const amountValue = parseFloat(inputAmountText);
     // clear input field
-    inputField = '';
-    return inputValue;
+    inputField.value = '';
+    return amountValue;
 }
-
 function updateTotalField(totalFieldId, amount) {
     const totalElements = document.getElementById(totalFieldId);
-    const totalText = totalElements.value;
+    const totalText = totalElements.innerText;
     const previousTotal = parseFloat(totalText);
     totalElements.innerText = previousTotal + amount;
 }
-
 function updateBalance(amount, isAdd) {
     const balanceTotal = document.getElementById('balance-total');
     const balanceTotalText = balanceTotal.innerText;
     const previousBalanceTotal = parseFloat(balanceTotalText);
-    if (isAdd = true) {
+    if (isAdd == true) {
         balanceTotal.innerText = previousBalanceTotal + amount;
     }
     else {
         balanceTotal.innerText = previousBalanceTotal - amount;
     }
 }
-
 document.getElementById('deposit-button').addEventListener('click', function () {
     const depositAmount = getInputValue('deposit-input');
     updateTotalField('deposit-total', depositAmount)
